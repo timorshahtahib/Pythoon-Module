eae9820dd389ecf8f5095d2e6a1b40542791e8c3
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 14:13:25 +0330

    folders

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import functions 
+#import functions 
+#from functions import say_hell,sum
+#from packes.functions import *
+import packes.functions as func
+from math import sqrt
 
 
-functions.say_hell('timor')
+#functions.say_hell('timor')
+func.say_hell('timor')
 
 
-functions.sum(2,3)
+#functions.sum(2,3)
+func.sum(2,3)
+
+
+print(sqrt(4))
